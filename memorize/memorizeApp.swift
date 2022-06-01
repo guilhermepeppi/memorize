e3a2d79a0c98cf2e43deca95c30a86c71af91281
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-06-01 09:18:04 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Guilherme Peppi on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
