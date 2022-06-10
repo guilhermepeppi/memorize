1dd0aa8cf2d971809feb0da2738a1bcb4ff73971
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-06-10 12:18:52 -0300

    Nothing

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -9,10 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello World!")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
